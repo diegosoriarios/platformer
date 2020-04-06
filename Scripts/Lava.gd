75169750d5db38810fc2f0f3f33c3d1d88332835
diegosoriarios <diegosoriarios@gmail.com>
2020-04-06 18:31:38 -0300

    Lava

---
+++ Scripts/Lava.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var areas = $Area2D.get_overlapping_bodies()
+	
+	for area in areas:
+		if area.name == "Player":
+			area.hit()
